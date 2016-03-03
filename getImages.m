b654c3dd206bb15e83ed9ee1c01ef7676959454c
Glen Brown <gbrown92@outlook.com>
2016-03-03 12:37:37 +0000

    Images now loading correctly.

--- getImages.m
+++ getImages.m
@@ -4,11 +4,14 @@ imagefiles = dir([directory '*.jpg']);
 n = length(imagefiles);
 
 for i=1:n
-   disp(imagefiles(i).name);
    img = imread([directory imagefiles(i).name]);
-   
-   if(size(size(img)) == 2)
-       img = reshape(img, [size(img) 1]);
+      
+   if(size(size(img), 2) == 2)
+       % Do not remove hackyFig! Otherwise an actual figure will appear
+       % when ind2rgb() is executed despite it not showing anything...
+       hackyFig = figure('Visible','off');
+       img = ind2rgb(img, gray);
+       close(hackyFig);
    end
    
    Images(:,:,:,i) = img;
--- main.m
+++ main.m
@@ -2,7 +2,11 @@ function [ tPos tNeg fPos fNeg ] = main( FEOptions, COptions, trainingDir, testi
 
 %% Training
 % Get all the training images.
+
+disp('Loading Training Images - Positive');
 trainingImagesPos = getImages('inputs\images\pos\');
+
+disp('Loading Training Images - Negative');
 trainingImagesNeg = getImages('inputs\images\neg\');
 
 
