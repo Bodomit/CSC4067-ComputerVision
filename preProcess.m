e17fd9c2381bf5802b335f46b74b79091d13f921
Glen Brown <gbrown92@outlook.com>
2016-03-31 13:49:32 +0100

    Added the colour normalisation pre-processor.

--- preProcess.m
+++ preProcess.m
@@ -3,6 +3,6 @@ function [ Images ] = preProcess( Images )
 for i=1:size(Images,4)
 
     % Images(:,:,:,i) = contrastEnhance(Images(:,:,:,i));
-    
+    % Images(:,:,:,i) = normaliseRGBImage(Images(:,:,:,i));
 end
 
